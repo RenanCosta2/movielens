948304cf34196350dae4202c9727b32d28560beb
RenanCosta2 <renanleitedacosta@gmail.com>
2026-03-11 14:58:27 -0300

    fix: conversão da coluna movie_id corretamente

--- sql/silver/silver_dim_movies.sql
+++ sql/silver/silver_dim_movies.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE movielens_silver.dim_movies AS
 
 SELECT 
-  movieId AS movie_id, 
+  SAFE_CAST(movieId AS INT64) AS movie_id, 
   REGEXP_REPLACE(title, r'\((\d{4})\)', '') AS title,
   REGEXP_EXTRACT(title, r'\((\d{4})\)') AS year
 FROM 
